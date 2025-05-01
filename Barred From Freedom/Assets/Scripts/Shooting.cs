ef92d749e1a549e6a1dfc40131dea419263ed30a
Xperience007 <tntbiscuit456@gmail.com>
2025-04-30 22:03:37 -0500

    Added hitscan to gun, needs some fixing

--- Barred From Freedom/Assets/Scripts/Shooting.cs
+++ Barred From Freedom/Assets/Scripts/Shooting.cs
@@ -11,6 +11,19 @@ public class Shooting : MonoBehaviour
 
     bool hasAttacked = false;
     public float attackTime = 1.0f;
+    public static float damage = 15.0f;
+    float slowTimer = 3.0f;
+    private bool collided = false;
+
+    public Transform FirePoint;
+    public GameObject Fire;
+    public GameObject HitPoint;
+
+    public AudioSource myAudio;
+    public AudioSource hitAudio;
+    public AudioClip shootSound;
+    public AudioClip hitSound;
+
 
     private void Awake()
     {
@@ -20,14 +33,23 @@ public class Shooting : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0) && !hasAttacked)
+        ItemMenu itemMenu = FindObjectOfType<ItemMenu>();
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10f;
+        mousePos = mainCamera.ScreenToWorldPoint(mousePos);
+        Debug.DrawRay(transform.position, mousePos - transform.position, Color.blue);
+
+        if (Input.GetMouseButtonDown(0) && !hasAttacked && !itemMenu.inMenu && !PauseMenu.GameIsPaused)
         {
+            myAudio.PlayOneShot(shootSound);
+            Hitscan();
+
             hasAttacked = true;
             player.isShooting = true;
             animator.SetBool("isShooting", true);
@@ -48,5 +70,48 @@ public class Shooting : MonoBehaviour
     private void AttackReset()
     {
         hasAttacked = false;
+    }
+
+    public void Hitscan()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        int layerMask = ~LayerMask.GetMask("Player");
+
+        if (Physics.Raycast(ray, out hit, 999f, layerMask))
+        {
+            GameObject a = Instantiate(Fire, FirePoint.position, Quaternion.identity);
+            GameObject b = Instantiate(HitPoint, hit.point, Quaternion.identity);
+
+            Destroy(a, 1);
+            Destroy(b, 1);
+
+            if (hit.collider.CompareTag("Enemy") || hit.collider.CompareTag("Enemy1") || hit.collider.CompareTag("Enemy2"))
+            {
+                hitAudio.PlayOneShot(hitSound);
+                collided = true;
+                EnemyAI enemy = hit.collider.GetComponent<EnemyAI>();
+                enemy.Enemy1TakeDamage(damage);
+                EnemyAI.hasTakenDamage = true;
+                if (playerController.isSlowed)
+                {
+                    enemy.agent.speed -= 0.5f;
+                    if (slowTimer > 0)
+                    {
+                        slowTimer -= Time.deltaTime;
+                    }
+                    else
+                    {
+                        enemy.agent.speed += 0.5f;
+                        slowTimer = 3.0f;
+                    }
+                }
+            }
+            else
+            {
+                EnemyAI.hasTakenDamage = false;
+            }
+        }
     }
 }
